1ba7c56af37c0cee7bac58bd0e4f8cfdb1a49d07
Jennifer Soule <jensoule@hotmail.com>
2022-02-22 20:04:19 -0700

    Onpost request review

--- src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Basics.cshtml.cs
@@ -11,6 +11,30 @@ namespace WebApp.Pages.SamplePages
         public string MyName;
         //properties
 
+        //the annotation [Temp Data] stores data until its read in another 
+        //immediate request
+        //this annotation has two methods called Keep(string) and
+        //Peek(string) (used on Contect page)
+        //kept in a dictionary (name/value pairs)
+        //useful to redirect when data is required fo rmore than a single
+        //request 
+        //Implemented by providers using either cookies or session state
+        //IMPORTANT -TempData is NOT  bound to any particular control like BindProperty
+
+        public string Feedback { get; set; }
+
+
+        //annotation of[BindProperty], ties a property in the PageModel class, directly to the a control on the Content page
+        //there is a one to one association 
+        //data is transferred between the two automatically
+        //on teh content page the control to use this property will have 
+        //  a helpertag call asp-for
+        //
+        //to retain the value in the control, tied to this property AND retained 
+        //  via the @page use the SupportsGet attribute = true;
+        [BindProperty(SupportsGet = true)]
+        public int id { get; set; }
+
         //constructors
 
         //behaviours aka methods
@@ -36,6 +60,44 @@ namespace WebApp.Pages.SamplePages
             {
                 MyName = null;
             }
+        }
+
+        //processing in response toa request from a form on a web page
+        //is normally referred to as a POST (method="post")
+
+        //General Post (OnPost())
+        //a general post occurs whenan asp-page-handler is NOT used
+        //theh return datatype can be void, however, you will normally
+        //encounter the datatype IActionResult
+        //the IActionResult requires some type of request action
+        // on the return statemetn of the method OnPost()
+        //typical actions:
+        //Page()
+        //  :does NOT issue a OnGet request
+        // :remaind on teh current page
+        //  :a good action for form processing involving validation
+        //      and with the catch of a try/catch
+        //  RedirectToPage()
+        //  :DOES issue an OnGet request
+        //  :is used to retain input value via the @page anf your BindProperty
+        //          for controls on your form in the Content page
+
+
+        public IActionResult OnPost()
+        {
+            //This line of code is used to cause a delay in processing
+            //so we can see on the Network Activity some type of
+            //simulated processing
+            Thread.Sleep(2000);
+
+            //retrieves data via the Request Obj
+            //  Request: web page to server
+            //  Response: serber to web page
+            string buttonvalue = Request.Form["theButton"];
+            Feedback = $"BUtton pressed is {buttonvalue} with numeric input of {id}";
+            //  return Page(); //does NOT issue an OnGet(request)
+            //return RedirectToPage(); //issues a request for OnGet()
+            return RedirectToPage(new { id = id }); //creates anaonymous obj
         }
     }
 }
