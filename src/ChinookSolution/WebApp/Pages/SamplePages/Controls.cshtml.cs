d8edf945c1780acb9777d51832bcec9bd73341c1
Jennifer Soule <jensoule@hotmail.com>
2022-03-02 10:52:07 -0700

    update controls page

--- src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/SamplePages/Controls.cshtml.cs
@@ -7,35 +7,38 @@ namespace WebApp.Pages.SamplePages
     {
         [TempData]
         public string Feedback { get; set; }
-
         [BindProperty]
         public string EmailText { get; set; }
         [BindProperty]
         public string PasswordText { get; set; }
         [BindProperty]
-        public DateTime DateTimeText { get; set; } = DateTime.Today;
-        //public string DateTimeText { get; set; }
-        public string[] RadioMeals = new[] { "breakfast", "lunch", "dinner/supper", "snacks" };
-        //[BindProperty]
-        //public string RadioMeal { get; set; }
+        //public DateTime DateTimeText { get; set; } = DateTime.Today;
+        public string DateTimeText { get; set; }
         [BindProperty]
-        public bool AcceptanceBox { get; set; } //remember value =  true on input control
+        public string RadioMeal { get; set; }
+        public string[] Meals { get; set; } = new[] { "breakfast", "lunch", "dinner/supper", "snacks" };
+        [BindProperty]
+        public bool AcceptanceBox { get; set; }
         [BindProperty]
         public string MessageText { get; set; }
+
         [BindProperty]
         public int MyRide { get; set; }
-        //pretend the collection is coing from the database
-        //each row of the collection has two values: seletion value; selection txt
-        //the class SelectionList will be used as the datatype for the collection
+        //pretend the collection is coming from the database
+        //each row of the collection has two values: selection value; selection text
+        //the class SelectionList will be use as the datatype for the collection
+        //the will need to be recreate on each internet trip
         public List<SelectionList> Rides { get; set; }
+
         [BindProperty]
         public string VacationSpot { get; set; }
-        public List<String> VacationSpotList {get; set;}
+        //pretend the collection is coming from the database
+        //create a List<string> to be used on the <datalist>
+        public List<string> VacationSpots { get; set; }
 
         [BindProperty]
-        public int RangeValue { get; set; }
-
-
+        // input type=range
+        public int ReviewRating { get; set; }
         public void OnGet()
         {
             PopulateLists();
@@ -43,49 +46,55 @@ namespace WebApp.Pages.SamplePages
 
         public void PopulateLists()
         {
-            //pretending that this data comes from the database
+            //used for the drop down list
             Rides = new List<SelectionList>();
-            Rides.Add(new SelectionList() { ValidId = 3, DisplayText = "Bike" });
-            Rides.Add(new SelectionList() { ValidId = 5, DisplayText = "Board" });
-            Rides.Add(new SelectionList() { ValidId = 2, DisplayText = "Bus" });
-            Rides.Add(new SelectionList() { ValidId = 1, DisplayText = "Car" });
-            Rides.Add(new SelectionList() { ValidId = 4, DisplayText = "Motorcycle" });
+            Rides.Add(new SelectionList() { ValueId = 1, DisplayText = "Car" });
+            Rides.Add(new SelectionList() { ValueId = 2, DisplayText = "Bus" });
+            Rides.Add(new SelectionList() { ValueId = 3, DisplayText = "Bike" });
+            Rides.Add(new SelectionList() { ValueId = 4, DisplayText = "Motorcycle" });
+            Rides.Add(new SelectionList() { ValueId = 5, DisplayText = "Board" });
+            Rides.Sort((x, y) => x.DisplayText.CompareTo(y.DisplayText)); //ascending
+            //Rides.Sort((x, y) => y.DisplayText.CompareTo(x.DisplayText)); //descending
 
-            VacationSpotList = new List<string>();
-            VacationSpotList.Add("California");
-            VacationSpotList.Add("Caribbean");
-            VacationSpotList.Add("Cruising");
-            VacationSpotList.Add("Europe");
-            VacationSpotList.Add("Florida");
-            VacationSpotList.Add("Mexico");
+            //used for datalist control
+            VacationSpots = new List<string>();
+            VacationSpots.Add("California");
+            VacationSpots.Add("Caribbean");
+            VacationSpots.Add("Cruising");
+            VacationSpots.Add("Europe");
+            VacationSpots.Add("Florida");
+            VacationSpots.Add("Mexico");
+
         }
-
         public IActionResult OnPostText()
         {
-            //this method is tied to teh specific button on the form via
-            //  the asp-page-handler attribute
-            //the form of teh method name is OnPost then concatenate the
-            //value given to the handler attribute
-
-            Feedback = $"Email {EmailText}; Password{PasswordText}; Date{DateTimeText}";
+            //echo back the input values
+            Feedback = $"Email {EmailText}; Password {PasswordText}; Date {DateTimeText}";
             return Page();
         }
-
         public IActionResult OnPostRadioCheckArea()
         {
-            Feedback = $"Meal {RadioMeals}; Acceptance{AcceptanceBox}; Message{MessageText}";
+            //echo back the input values
+            Feedback = $"Meal {RadioMeal}; Acceptance {AcceptanceBox}; Message {MessageText}";
             return Page();
         }
+
         public IActionResult OnPostListSlider()
         {
-            Feedback = $"Ride {MyRide}; Vacation {VacationSpot}; Review Satification {RangeValue}";
+            //echo back the input values
+            Feedback = $"Ride {MyRide}; Vacation Spot {VacationSpot}; Control Review Rating {ReviewRating}";
+            //on each post, reload the lists
             PopulateLists();
             return Page();
         }
-        public class SelectionList
-        {
-            public int ValidID { get; set; }
-            public string DisplayText { get; set; }
-        }
+
+    }
+
+    public class SelectionList
+    {
+        //this class is designed for a <select> tag (dropdownlist) which needs a
+        //value item and a display item
+        public int ValueId { get; set; } //will be the returned value
+        public string DisplayText { get; set; } //will be the text display
     }
 }
