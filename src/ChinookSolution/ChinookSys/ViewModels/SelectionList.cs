83edb5bb185238a142b3f92dd1506e86cd82b8d2
Jennifer Soule <jensoule@hotmail.com>
2022-03-05 15:07:11 -0700

    create viewmodel for albumsbylist and selectionlist

---
+++ src/ChinookSolution/ChinookSys/ViewModels/SelectionList.cs
@@ -0,0 +1,18 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+#endregion
+
+namespace ChinookSys.ViewModels
+{
+    public class SelectionList
+    {
+        public int ValueId { get; set; }
+        public string DisplayText { get; set; }
+    }
+}
--- src/ChinookSolution/WebApp/Pages/Index.cshtml.cs
+++ src/ChinookSolution/WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿#nullable disable
+using ChinookSys.BLL;
+using ChinookSys.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#region Additional Namespaces
+#endregion
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        #region Private variable ad DI constructor
         private readonly ILogger<IndexModel> _logger;
+        private readonly AboutServices _aboutServices;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, 
+                            AboutServices aboutservices)
         {
             _logger = logger;
+            _aboutServices = aboutservices;
         }
+        #endregion
 
+        #region Feedback and Errorhandling
+        [TempData]
+        public string Feedback { get; set; }
+
+        public bool HasFeedback => !string.IsNullOrWhiteSpace(Feedback);
+        #endregion
         public void OnGet()
         {
-
+            //consume a service
+            DbVersionInfo info = _aboutServices.GetDbVersion();
+            if (info == null)
+            {
+                Feedback = "Version Unknown";
+            }
+            else
+            {
+                Feedback = $"version: {info.Major}.{info.Minor}.{info.Build} " + $"Release date of{info.ReleaseDate.ToShortDateString()}";
+            }
         }
     }
 }
